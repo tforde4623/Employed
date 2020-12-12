987d3e476cc94af6c6c9ec96417f0999d14cbb8f
tt4623 <sltebr33zy@gmail.com>
2020-12-11 21:27:46 -0600

    main functions for flow of CLI

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const prompts = require("./lib/prompts")
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,7 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const { manager } = require("./lib/prompts");
+const prompts = require("./lib/prompts");
 const { resolve } = require("path");
 const { RSA_X931_PADDING } = require("constants");
 
@@ -55,7 +54,7 @@ const promptEmployeeNext = () => {
 }
   
 // ask what kind of employee to create (using list)
-const chooseEmployee = (role) => {
+const chooseEmployee = role => {
   if (role === 'Engineer') {
     createEngineer();
   } else if (role === 'Intern') {
